cdb54b2f7146ce98bc69381130c1af1893c8ecba
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-05-27 17:11:01 -0400

    moved test files

--- texbld/common/directory.py
+++ texbld/common/directory.py
@@ -7,7 +7,7 @@ CACHE_BASE = os.path.join(Path.home(), ".cache", "texbld")
 
 
 if "pytest" in sys.modules:
-    dirname = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "pytest_files")
+    dirname = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "test_files")
     print("works", dirname)
     CONFIG_BASE = os.path.join(dirname, "config")
     CACHE_BASE = os.path.join(dirname, "cache")
