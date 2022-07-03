57a938c9e0337e7e75b7c7ba5968f136c6465643
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-07-03 11:13:29 -0400

    sha256 test update

--- texbld/cli/getsha256_test.py
+++ texbld/cli/getsha256_test.py
@@ -7,7 +7,7 @@ from texbld.cli.getsha256 import getsha256
 def test_works():
     resource = "github:octocat/Hello-World#553c2077f0edc3d5dc5d17262f6aa498e69d6f8e"
     sha256 = "cc82fedd92d6fd783c9c6f984e4a67daa6c03f783e47016ba3610691b542df92"
-    assert getsha256(resource) == sha256
+    assert getsha256(resource)[1] == sha256
 
 
 def test_nongithub_fails():
