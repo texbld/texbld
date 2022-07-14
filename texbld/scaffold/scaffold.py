7af178e86f52ea7ad30d5ecc4281220ef0550f15
Juni Kim <junikimm717@gmail.com>
2022-07-14 16:53:05 -0400

    removed shutil.copytree from image scaffold
    On NixOS systems, the previous method resulted in a permission denied
    error.

--- texbld/scaffold/scaffold.py
+++ texbld/scaffold/scaffold.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 from texbld.common.image import Image
 from texbld.config import PROJECT_CONFIG_FILE
 from texbld.scaffold.copy import copy_image
@@ -29,9 +28,7 @@ def scaffold_image(directory: str):
     name = os.path.basename(directory)
     if os.path.exists(directory):
         raise FileExistsError(directory)
-    os.makedirs(os.path.dirname(directory), exist_ok=True)
-    src = os.path.join(os.path.dirname(__file__), "sample_image")
-    shutil.copytree(src, directory)
-    text = open(os.path.join(directory, "image.toml")).read()
-    open(os.path.join(directory, "image.toml"), "w").write(
-        text.replace("image_name", name))
+    os.makedirs(directory)
+    src_path = os.path.join(os.path.dirname(__file__), "sample_image.toml")
+    text = open(src_path).read()
+    open(os.path.join(directory, "image.toml"), "w").write(text.replace("image_name", name))
