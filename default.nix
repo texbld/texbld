57ef28cf2778e533538b3ed00a4cb8a8d62a2b8f
Juni Kim <junikimm717@gmail.com>
2022-07-18 22:27:00 -0400

    support for other architectures

--- default.nix
+++ default.nix
@@ -1,13 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 pkgs.python3.pkgs.buildPythonPackage rec {
   pname = "texbld";
   version = "0.4.dev0";
   src = ./.;
   format = "pyproject";
 
-  propagatedBuildInputs = with pkgs.python3.pkgs;
-      [jsonschema docker requests toml urllib3];
-  buildInputs = with pkgs; [poetry];
+  propagatedBuildInputs = with pkgs.python3.pkgs; [
+    jsonschema
+    docker
+    requests
+    toml
+    urllib3
+  ];
+  buildInputs = with pkgs; [ poetry ];
   meta = with pkgs.lib; {
     homepage = "https://texbld.com";
     description = "A Modern Build Tool for Your Papers";
