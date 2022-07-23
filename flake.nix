68606ec6c6fd03a32f8315def818610fa0ea35a0
Juni Kim <junikimm717@gmail.com>
2022-07-23 08:14:33 -0400

    cross-platform nix expression

--- flake.nix
+++ flake.nix
@@ -1,46 +1,52 @@
 {
-  description = "A very basic flake";
+  description = "A Modern Build Tool for Your Papers";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: 
-  let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in
-  {
-    defaultPackage.x86_64-linux = 
-      pkgs.python3.pkgs.buildPythonPackage rec {
-        pname = "texbld";
-        version = "0.3.0";
-        src = ./.;
-        format = "pyproject";
-        propagatedBuildInputs = with pkgs.python3.pkgs;
-            [jsonschema docker requests toml urllib3];
-        buildInputs = with pkgs; [poetry];
-        meta = with pkgs.lib; {
-          homepage = "https://texbld.com";
-          description = "A Modern Build Tool for Your Papers";
-          license = licenses.gpl3;
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib;
+    eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        defaultPackage = pkgs.python3.pkgs.buildPythonPackage rec {
+          pname = "texbld";
+          version = "0.3.0";
+          src = ./.;
+          format = "pyproject";
+          propagatedBuildInputs = with pkgs.python3.pkgs; [
+            jsonschema
+            docker
+            requests
+            toml
+            urllib3
+          ];
+          buildInputs = with pkgs; [ poetry ];
+          meta = with pkgs.lib; {
+            homepage = "https://texbld.com";
+            description = "A Modern Build Tool for Your Papers";
+            license = licenses.gpl3;
+          };
+          doCheck = false;
         };
-        doCheck = false;
-      };
-    devShell.x86_64-linux =
-      let
-        python_dev_packages = (
-          let
-            deps = pythonpkgs: with pythonpkgs; 
-              [autopep8 pylint poetry];
-          in pkgs.python3.withPackages deps
-        );
-      in
-      pkgs.mkShell {
-        buildInputs = [python_dev_packages pkgs.scc];
-        shellHook = ''
-          if ! poetry env info -p 2>&1 > /dev/null; then
-            poetry install
-          fi
-          source `poetry env info -p`/bin/activate
-        '';
-      };
-  };
+        devShell = let
+          python_dev_packages = (let
+            deps = pythonpkgs:
+              with pythonpkgs; [
+                autopep8
+                pylint
+                poetry
+                jsonschema
+                docker
+                requests
+                toml
+                urllib3
+                pytest
+                pytest-xdist
+              ];
+          in pkgs.python3.withPackages deps);
+        in pkgs.mkShell { buildInputs = [ python_dev_packages pkgs.scc ]; };
+
+        formatter = nixpkgs.legacyPackages."${system}".nixfmt;
+      });
 }
