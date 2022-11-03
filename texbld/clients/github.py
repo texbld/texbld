92b7718fbb2a02bf5ca9727d78f1fa9b30f5432d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 04:21:30 +0000

    Adding tarfile member sanitization to extractall()

--- texbld/clients/github.py
+++ texbld/clients/github.py
@@ -70,7 +70,26 @@ class GitHubClient(Client):
             for m in members:
                 m.path = m.path[len(original_dir):]
             os.makedirs(self.decompressed_dir(), exist_ok=True)
-            archive.extractall(path=self.decompressed_dir(), members=members)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, path=self.decompressed_dir(), members=members)
 
     def get_browser(self) -> 'ImageFsBrowser':
         self.browser = ImageFsBrowser(
